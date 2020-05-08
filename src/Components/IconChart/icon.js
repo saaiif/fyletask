04c48b4a888fb4d80470d8312c0a4519d68275af
Saif <mujawarmsaif@gmail.com>
2020-05-08 19:24:46 +0530

    updated

--- src/Components/IconChart/icon.js
+++ src/Components/IconChart/icon.js
@@ -8,49 +8,49 @@ export default function Icon() {
 					<span>This is heading</span>
 					<p>In two lines</p>
 				</div>
-				<div class="wrapper">
-					<div class="panel pricing-table">
-						<div class="features-plan">
-							<ul class="features-names">
-								<li class="pricing-features-list">Lorem Ipsum 1</li>
-								<li class="pricing-features-list">Lorem Ipsum 2</li>
-								<li class="pricing-features-list">Lorem Ipsum 3</li>
-								<li class="pricing-features-list">Lorem Ipsum 4</li>
-								<li class="pricing-features-list">Lorem Ipsum 5</li>
-								<li class="pricing-features-list">Lorem Ipsum 6</li>
-								<li class="pricing-features-list">Lorem Ipsum 7</li>
-								<li class="pricing-features-list">Lorem Ipsum 8</li>
-								<li class="pricing-features-list">Lorem Ipsum 9</li>
+				<div className="wrapper">
+					<div className="panel pricing-table">
+						<div className="features-plan">
+							<ul className="features-names">
+								<li className="pricing-features-list">Lorem Ipsum 1</li>
+								<li className="pricing-features-list">Lorem Ipsum 2</li>
+								<li className="pricing-features-list">Lorem Ipsum 3</li>
+								<li className="pricing-features-list">Lorem Ipsum 4</li>
+								<li className="pricing-features-list">Lorem Ipsum 5</li>
+								<li className="pricing-features-list">Lorem Ipsum 6</li>
+								<li className="pricing-features-list">Lorem Ipsum 7</li>
+								<li className="pricing-features-list">Lorem Ipsum 8</li>
+								<li className="pricing-features-list">Lorem Ipsum 9</li>
 							</ul>
 						</div>
 
-						<div class="pricing-plan">
-							<h2 class="pricing-header">Lollipop</h2>
-							<ul class="pricing-features">
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
-								<li class="pricing-features-item">&#10003;</li>
+						<div className="pricing-plan">
+							<h2 className="pricing-header">Lollipop</h2>
+							<ul className="pricing-features">
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
+								<li className="pricing-features-item">&#10003;</li>
 							</ul>
 						</div>
 
-						<div class="pricing-plan">
-							<h2 class="pricing-header">Ice candy</h2>
-							<ul class="pricing-features">
-								<li class="pricing-features-item">&#215;</li>
-								<li class="pricing-features-item">&#215;</li>
-								<li class="pricing-features-item">&#215;</li>
-								<li class="pricing-features-item red">&#215;</li>
-								<li class="pricing-features-item red">&#215;</li>
-								<li class="pricing-features-item red">&#215;</li>
-								<li class="pricing-features-item red">&#215;</li>
-								<li class="pricing-features-item">&#215;</li>
-								<li class="pricing-features-item">&#215;</li>
+						<div className="pricing-plan">
+							<h2 className="pricing-header">Ice candy</h2>
+							<ul className="pricing-features">
+								<li className="pricing-features-item">&#215;</li>
+								<li className="pricing-features-item">&#215;</li>
+								<li className="pricing-features-item">&#215;</li>
+								<li className="pricing-features-item red">&#215;</li>
+								<li className="pricing-features-item red">&#215;</li>
+								<li className="pricing-features-item red">&#215;</li>
+								<li className="pricing-features-item red">&#215;</li>
+								<li className="pricing-features-item">&#215;</li>
+								<li className="pricing-features-item">&#215;</li>
 							</ul>
 						</div>
 					</div>
