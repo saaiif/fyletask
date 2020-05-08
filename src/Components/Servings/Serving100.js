28d7c288c2a66e9333678ececfe6e535440aa575
Saif <mujawarmsaif@gmail.com>
2020-05-08 19:30:46 +0530

    updated

--- src/Components/Servings/Serving100.js
+++ src/Components/Servings/Serving100.js
@@ -37,7 +37,9 @@ class Serving100 extends Component {
 							<p>For 100 serving</p>
 						</div>
 						<div class="card active">
-							<h6 style={margin}>Your saving with lollipop</h6>
+							<div className="endTitile">
+								<h6>Your saving with lollipop</h6>
+							</div>
 
 							{/* <h4>Michael Pearson</h4> */}
 							<div className="mainSub" style={colors}>
